cac669bde33865cd8d64eb626de2297158c38401
Karthiven <getkarven@gmail.com>
2020-04-14 20:55:18 +0530

    login funct for amazone working need jusr rem=naming and ordering

--- Test/Base/basetwo.java
+++ Test/Base/basetwo.java
@@ -11,12 +11,81 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import PageFactory.Acti_Login;
+import Pf.Amsignemail;
+import Pf.Amsignpass;
 import Utility.Properties;
 import Utility.Wait;
 
 public class basetwo { 
 	
 	public static WebDriver driver=null;
+	Amsignemail empage;
+	Amsignpass pwpage;
+	
+	
+	
+	public void Acti_Setup() throws IOException, InterruptedException
+	{
+		
+		Properties prop=new Properties("Envirnoment");
+		
+		
+		
+		if(prop.getdata("env").equals("chrome"))
+		{
+			
+		ChromeOptions option=new ChromeOptions();
+		option.addArguments("start-maximized");
+		option.addArguments("disable-infobars");
+		System.setProperty( "webdriver.chrome.driver",prop.getdata("chromedriver"));
+		driver=new ChromeDriver(option);
+		Thread.sleep(3000);
+		
+		}
+		
+		else if(prop.getdata("env").equals("Firefox"))
+		{
+			
+			System.setProperty("webdriver.gecko.driver", prop.getdata("firefoxdriver"));
+			driver=new FirefoxDriver();	
+			
+		}
+		Thread.sleep(2000);
+		
+	}
+	
+	
+	@BeforeMethod
+	public void Launch() throws IOException, InterruptedException
+	{
+		
+		Acti_Setup();
+		
+		Properties prop=new Properties("data2");
+		driver.get(prop.getdata("url"));
+	
+		
+		 empage= new Amsignemail();
+		 empage.getemail().sendKeys(prop.getdata("username"));
+		 empage.getbut().click();
+		 
+		 pwpage=new Amsignpass();
+		 pwpage.getpass().sendKeys(prop.getdata("password"));
+		 pwpage.getinbut().click();
+		 
+		 
+		
+		
+
+	}
+	
+	
+	
+	@AfterMethod
+	public void close()
+	{
+		driver.close();
+	}
 	
 
 
