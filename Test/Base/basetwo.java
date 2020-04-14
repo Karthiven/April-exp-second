a58eae20b1c99f1829632cd30b00edd7cbf45274
Karthiven <getkarven@gmail.com>
2020-04-14 18:49:57 +0530

    nm

--- Test/Base/basetwo.java
+++ Test/Base/basetwo.java
@@ -1,13 +1,24 @@
 package Base;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import PageFactory.Acti_Login;
+import Utility.Properties;
+import Utility.Wait;
 
 public class basetwo { 
 	
 	public static WebDriver driver=null;
 	
-	// hi just a commend
-	
-	// second commend
+
+
 
 }
